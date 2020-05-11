146e0bb3079bef82d1ba1bd299aacaac7090f47d
renan bet rodrigues <renanbet@hotmail.com>
2020-05-11 03:35:23 -0300

    config

--- cli.sh
+++ cli.sh
@@ -6,16 +6,26 @@ then
 
   while IFS== read var1 var2; do
     git clone "https://github.com/renanbet/$var1.git"
-    # cd tests
-    # git clone "https://github.com/renanbet/$var1.git"
-    # cd ..
+    cd tests
+    git clone "https://github.com/renanbet/$var1.git"
+    cd ..
   done < "$arquivo"
 
   arquivo="config.env"
 
   while IFS== read var1 var2; do
       sed -i "s/#$var1/$var2/" docker-compose.yml
-      # sed -i "s/#$var1/$var2/" tests/docker-compose.yml
+      sed -i "s/#$var1/$var2/" tests/docker-compose.yml
+  done < "$arquivo"
+elif [ "$1" = "env" ]
+then
+  arquivo="config.env"
+
+  while IFS== read var1 var2; do
+  echo $var1
+  echo $var2
+      sed -i "s/#$var1/$var2/" docker-compose.yml
+      sed -i "s/#$var1/$var2/" tests/docker-compose.yml
   done < "$arquivo"
 elif [ "$1" = "pull" ]
 then
